a993bb74edda881cb882ea60e21ce6c8b58b0722
Tharunfitfun <tharunfitfun204@gmail.com>
2025-04-05 07:56:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -72,7 +72,7 @@ PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "False") == "True" e
 DISABLE_CHANNEL_BUTTON = os.environ.get("DISABLE_CHANNEL_BUTTON", None) == 'True'
 
 BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
-USER_REPLY_TEXT = "<b>ᴏᴏᴘs~ {mention} ᴏɴʟʏ ᴍʏ ᴍᴀꜱᴛᴇʀ ᴄᴀɴ ꜱᴛᴏʀᴇ ꜰɪʟᴇꜱ ɪɴ ᴍᴇ~  ʏᴏᴜ'ʀᴇ ɴᴏᴛ ʜɪᴍ, ʙᴀʙʏ~<\b>"
+USER_REPLY_TEXT = "<b>ᴏᴏᴘs~ 🙃 ᴏɴʟʏ ᴍʏ ᴍᴀꜱᴛᴇʀ ᴄᴀɴ ꜱᴛᴏʀᴇ ꜰɪʟᴇꜱ ɪɴ ᴍᴇ~  ʏᴏᴜ'ʀᴇ ɴᴏᴛ ʜɪᴍ, ʙᴀʙʏ~<\b>"
 
 ADMINS.append(OWNER_ID)
 ADMINS.append(1250450587)
