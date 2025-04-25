2ce180edb6b571f93043a146e26abc38e259319a
Tharunfitfun <tharunfitfun204@gmail.com>
2025-04-25 17:59:33 +0530

    Update useless.py

--- plugins/useless.py
+++ plugins/useless.py
@@ -1,3 +1,4 @@
+#fixed 
 import time
 from bot import Bot
 from pyrogram.types import Message
@@ -6,23 +7,15 @@ from config import ADMINS, BOT_STATS_TEXT, USER_REPLY_TEXT
 from datetime import datetime
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from helper_func import get_readable_time
-from database import db  # Importing the db functions
 
 @Bot.on_message(filters.command('stats') & filters.user(ADMINS))
 async def stats(bot: Bot, message: Message):
-    # Calculate bot uptime
     now = datetime.now()
     delta = now - bot.uptime
-    uptime = get_readable_time(delta.seconds)
-
-    # Fetch stats from the database
-    users = await db.total_users_count()
-    files = await db.total_files_count()
-
-    # Format and send the stats message
-    await message.reply(BOT_STATS_TEXT.format(users=users, files=files, uptime=uptime))
+    time = get_readable_time(delta.seconds)
+    await message.reply(BOT_STATS_TEXT.format(uptime=time))
 
 @Bot.on_message(filters.private & filters.incoming)
-async def useless(_, message: Message):
+async def useless(_,message: Message):
     if USER_REPLY_TEXT:
         await message.reply(USER_REPLY_TEXT)
